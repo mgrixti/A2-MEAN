aa1bb9e816ddd775fd3808d61fca6ebd0fa04f97
Matthew Grixti <matthewpcgrixti@gmail.com>
2016-03-17 22:16:55 -0600

    Set up API route

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
-/**
- * Created by Balatbat on 2016-03-17.
- */
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+var port = 80;
+var router = express.Router();
+
+
+// ----------- API ROUTES -------------
+// Route for API about/how to use page
+router.get('/', function(req, res){
+    res.sendfile('views/api.html');
+});
+
+// all paths on router will have /api/ added to their route
+app.use('/api', router);
+
+
+// --------- APPLICATION ROUTES ---------
+// Route for SPA
+app.get('/', function(req, res){
+    res.send('<h1>Angular app goes here</h1>');
+});
+
+console.log('API running on port: ' + port);
+app.listen(port);
