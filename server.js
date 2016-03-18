2a67acbfa0e10aa2d51845f456c44f739b7b16f3
Matthew Grixti <matthewpcgrixti@gmail.com>
2016-03-18 01:03:09 -0600

    get all todo and delete specific todo working.

--- server.js
+++ server.js
@@ -4,13 +4,41 @@ var app = express();
 
 var port = 80;
 var router = express.Router();
+var employee = require('./models/employees.js');
 
+mongoose.connect('mongodb://localhost/assign2');
 
 // ----------- API ROUTES -------------
 // Route for API about/how to use page
 router.get('/', function(req, res){
     res.sendfile('views/api.html');
 });
+
+router.route('/employees')
+    .get( function(req, res) {
+        employee.find({}, function (err, data){
+            res.json(data)
+        });
+    });
+
+// Get 'to-do' by the  employee's ID
+router.route('/todo/:employeeID')
+    .get(function(req, res) { // Get all 'todo's for a single employee
+        employee.find({id: req.params.employeeID}, 'todo', function (err, data){
+            res.json(data)
+    });
+});
+
+
+router.route('/todo/:employeeID/:todoID')
+    // Delete specif to-do item for an employee
+    .delete(function(req,res){
+        employee.update({id: req.params.employeeID},
+            {$pull: {todo: {id: req.params.todoID}}},
+            function(err, result){
+                res.send(result);
+        });
+    });
 
 // all paths on router will have /api/ added to their route
 app.use('/api', router);
