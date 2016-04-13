72fca527ebd7deb2e38d6c454b74f8f2a140cd59
abcd134 <jbala972@mtroyal.ca>
2016-04-12 20:58:05 -0600

    Added body parser and improved login page

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var port = 80;
+var port = 23423;
 var router = express.Router();
 var employee = require('./models/employees.js');
 
 app.use( bodyParser.urlencoded({
     extended:true
+
 }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // app.use(express.urlencoded());
 
